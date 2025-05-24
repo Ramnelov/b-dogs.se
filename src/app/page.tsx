4a6aaf917294005d329b5e44edd523bd164e2fd1
Felix Ramnelöv <felra653@student.liu.se>
2025-05-24 20:47:02 +0200

    Fixed text center error.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
       <div className="text-6xl">B-Dogs</div>
       <div className="text-3xl">Never put B-Dog in a corner.</div>
     </div>
